d2f2ae783b8964c2a99185bca8aa6b088a13d076
masddika <mf.mahardika@gmail.com>
2022-09-02 02:21:52 +0700

    savir reconf

--- savirconfig/enb.sh
+++ savirconfig/enb.sh
@@ -3,7 +3,7 @@
 LOG_ARGS="--log.all_level=debug"
 
 ENB_LOC="/home/adaptive/srsRAN/build/srsenb/src/"
-CONF_LOC="/home/adaptive/Open5Gs-srsran/"
+CONF_LOC="/home/adaptive/Open5Gs-srsran/savirconfig/"
 
 OTHER_ARGS="--enb_files.rr_config="${CONF_LOC}"rr.conf 
 --enb_files.rb_config="${CONF_LOC}"rb.conf
--- savirconfig/ue.sh
+++ savirconfig/ue.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 UE_LOC="/home/adaptive/srsRAN/build/srsue/src/"
-CONF_LOC="/home/adaptive/fajar/ltevir.conf/"
+CONF_LOC="/home/adaptive/Open5Gs-srsran/savirconfig/"
 OTHER_ARGS="--gw.netns=ue1"
 
 sudo ${UE_LOC}srsue ${CONF_LOC}ue.conf ${OTHER_ARGS} $@
