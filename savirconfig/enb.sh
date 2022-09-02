782cd08f14f1a100112806575d74981185c7a6e8
Fajar <iyotalah@gmail.com>
2022-09-02 13:37:53 +0700

    gpp

--- savirconfig/enb.sh
+++ savirconfig/enb.sh
@@ -9,4 +9,4 @@ OTHER_ARGS="--enb_files.rr_config="${CONF_LOC}"rr.conf
 --enb_files.rb_config="${CONF_LOC}"rb.conf
 --enb_files.sib_config="${CONF_LOC}"sib.conf"
 
-sudo ${ENB_LOC}srsenb ${CONF_LOC}enb.conf ${OTHER_ARGS} ${LOG_ARGS}
+sudo ${ENB_LOC}srsenb ${CONF_LOC}enb.conf ${OTHER_ARGS} ${LOG_ARGS} $@
