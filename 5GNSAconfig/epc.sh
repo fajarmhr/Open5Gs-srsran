eff353002950dfd05a04e00dcb149355184801f9
masddika <mf.mahardika@gmail.com>
2022-09-03 11:25:11 +0700

    rapihin script v2

--- 5GNSAconfig/epc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-LOG_ARGS="--log.all_level=debug"
-EPC_LOC="/home/adaptive/srsRAN/build/srsepc/src/"
-CONF_LOC="/home/adaptive/fajar/newnsa.conf/"
-
-OTHER_ARGS="--hss.db_file="${CONF_LOC}"user_db.csv"
-
-sudo ${EPC_LOC}srsepc ${CONF_LOC}epc.conf ${LOG_ARGS} ${OTHER_ARGS}
--- script/enb.log.sh
+++ script/enb.log.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo
 echo "### Watch the live ENB log"
-tail /tmp/enb.log
+tail -f /tmp/enb.log
