076514a8df7f28754f5123cf48c6d435256a350e
Fajar <iyotalah@gmail.com>
2022-09-05 15:23:29 +0700

    tambah handover

---
+++ ZMQ_based/N2handov/enb1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+LOG_ARGS="--log.all_level=debug"
+
+ENB_LOC=`locate /src/srsenb | grep /srsenb/src/srsenb`
+CONF_LOC=`locate enb.conf | grep N2handov`
+RR_LOC=`locate rr1.conf | grep N2handov`
+RB_LOC=`locate rb.conf | grep N2handov`
+SIB_LOC=`locate sib.conf | grep N2handov`
+PORT_ARGS="tx_port=tcp://*:2101,rx_port=tcp://localhost:2100"
+
+ZMQ_ARGS="--rf.device_name=zmq --rf.device_args=\"${PORT_ARGS},id=enb,base_srate=23.04e6\""
+OTHER_ARGS="--enb_files.rr_config="${RR_LOC}"
+--enb_files.rb_config="${RB_LOC}"
+--enb_files.sib_config="${SIB_LOC}""
+
+sudo ${ENB_LOC} ${CONF_LOC} ${OTHER_ARGS} ${ZMQ_ARGS} ${LOG_ARGS} $@
--- ZMQ_based/savirconfig/ue.sh
+++ ZMQ_based/savirconfig/ue.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo ip netns add ue1
-echo "abaikan kalo ada warning, gapopo..."
+# sudo ip netns add ue1
+# echo "abaikan kalo ada warning, gapopo..."
 
 UE_LOC=`locate /src/srsue | grep /srsue/src/srsue`
 CONF_LOC=`locate ue.conf | grep savir`
-NETNS=`sudo ip netns list`
+NETNS=`sudo ip netns list | grep 1`
 OTHER_ARGS="--gw.netns="${NETNS}""
 
 sudo ${UE_LOC} ${CONF_LOC} ${OTHER_ARGS} $@
