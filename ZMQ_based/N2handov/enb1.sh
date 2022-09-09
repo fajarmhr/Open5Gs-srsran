3ab60136b69e03aae0892c201ef70e5e6cf278b8
Fajar <iyotalah@gmail.com>
2022-09-09 14:19:44 +0700

    N2 belum

--- ZMQ_based/N2handov/enb1.sh
+++ ZMQ_based/N2handov/enb1.sh
@@ -5,13 +5,9 @@ LOG_ARGS="--log.all_level=debug"
 ENB_LOC=`locate /src/srsenb | grep /srsenb/src/srsenb`
 CONF_LOC=`locate enb.conf | grep N2handov`
 RR_LOC=`locate rr1.conf | grep N2handov`
-RB_LOC=`locate rb.conf | grep N2handov`
-SIB_LOC=`locate sib.conf | grep N2handov`
 PORT_ARGS="tx_port=tcp://*:2101,rx_port=tcp://localhost:2100"
 
 ZMQ_ARGS="--rf.device_args=\"fail_on_disconnect=true,${PORT_ARGS},id=enb,base_srate=11.52e6\""
-OTHER_ARGS="--enb_files.rr_config="${RR_LOC}"
---enb_files.rb_config="${RB_LOC}"
---enb_files.sib_config="${SIB_LOC}""
+OTHER_ARGS="--enb_files.rr_config="${RR_LOC}""
 
 sudo ${ENB_LOC} ${CONF_LOC} ${OTHER_ARGS} ${ZMQ_ARGS} ${LOG_ARGS} $@
--- ZMQ_based/N2handov/enb2.sh
+++ ZMQ_based/N2handov/enb2.sh
@@ -5,14 +5,10 @@ LOG_ARGS="--log.all_level=info"
 ENB_LOC=`locate /src/srsenb | grep /srsenb/src/srsenb`
 CONF_LOC=`locate enb.conf | grep N2handov`
 RR_LOC=`locate rr2.conf | grep N2handov`
-RB_LOC=`locate rb.conf | grep N2handov`
-SIB_LOC=`locate sib.conf | grep N2handov`
 PORT_ARGS="tx_port=tcp://*:2201,rx_port=tcp://localhost:2200"
 
 ZMQ_ARGS="--rf.device_args=\"fail_on_disconnect=true,${PORT_ARGS},id=enb,base_srate=11.52e6\""
 OTHER_ARGS="--enb_files.rr_config="${RR_LOC}"
---enb_files.rb_config="${RB_LOC}"
---enb_files.sib_config="${SIB_LOC}"
 --enb.enb_id=0x19C 
 --enb.gtp_bind_addr=127.0.1.2 
 --enb.s1c_bind_addr=127.0.1.2"
