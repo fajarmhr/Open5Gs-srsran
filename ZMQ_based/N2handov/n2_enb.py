36229cd4e4d400fabb626b2b60c72b029a0bec69
Fajar <iyotalah@gmail.com>
2022-09-21 14:51:29 +0700

    edit n2

--- ZMQ_based/N2handov/n2_enb.py
+++ ZMQ_based/N2handov/n2_enb.py
@@ -74,9 +74,9 @@ class n2_enb(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 11.52e6
+        self.samp_rate = samp_rate = 1920000
         self.cell_gain1 = cell_gain1 = 1
-        self.cell_gain0 = cell_gain0 = 0.4
+        self.cell_gain0 = cell_gain0 = 0.3
 
         ##################################################
         # Blocks
@@ -84,7 +84,7 @@ class n2_enb(gr.top_block, Qt.QWidget):
         self._cell_gain1_range = Range(0, 1, 0.1, 1, 200)
         self._cell_gain1_win = RangeWidget(self._cell_gain1_range, self.set_cell_gain1, "'cell_gain1'", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._cell_gain1_win)
-        self._cell_gain0_range = Range(0, 1, 0.1, 0.4, 200)
+        self._cell_gain0_range = Range(0, 1, 0.1, 0.3, 200)
         self._cell_gain0_win = RangeWidget(self._cell_gain0_range, self.set_cell_gain0, "'cell_gain0'", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._cell_gain0_win)
         self.zeromq_req_source_1 = zeromq.req_source(gr.sizeof_gr_complex, 1, 'tcp://localhost:2001', 100, False, -1)
