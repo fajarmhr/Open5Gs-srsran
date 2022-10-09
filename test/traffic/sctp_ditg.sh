0b53f82f3d099dd469c44bfb982e1d35c5ea9e7a
masddika <mf.mahardika@gmail.com>
2022-10-09 23:46:03 +0700

    edit test script

--- test/traffic/sctp_ditg.sh
+++
@@ -1,9 +0,0 @@
-#ITGSend -a 10.45.0.1 -m RTTM -T SCTP 3 1 -rp 9030 -l send_log_file
-sudo ip netns exec ue1 ITGSend -a 10.45.0.1 -m RTTM -T SCTP 3 1 -rp 9030 -l send_log_file
-
-# NOTE
-# -m <meter> : ODWM (default), RTTM
-# -T <protocol> : udp (default), tcp, icmp, sctp, dccp
-# -t <duration> : (default 10000ms)
-# -rp <receiver port>
-# -sc <src port>
