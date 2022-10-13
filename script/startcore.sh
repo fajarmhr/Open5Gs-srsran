e6f8cb98ba4ec755986c740d32b52b09d74f21d9
masddika <mf.mahardika@gmail.com>
2022-10-14 02:02:53 +0700

    add disable n start script

--- script/startcore.sh
+++ script/startcore.sh
@@ -1,70 +1,59 @@
 #!/bin/bash
-echo "This script will start Open5Gs Core."
+echo "This script will start Open5Gs."
+echo
+read -s -p "Press ENTER to continue, or CTRL + C to exit"
+
 echo
 echo
 echo "**** Starting mmed... ****"
-open5gs-mmed
+sudo systemctl start open5gs-mmed
 
 echo "**** Starting sgwcd... ****"
-gnome-terminal
-open5gs-sgwcd
+sudo systemctl start open5gs-sgwcd
 
 echo "**** Starting smfd... ****"
-gnome-terminal
-open5gs-smfd
+sudo systemctl start open5gs-smfd
 
 echo "**** Starting amfd... ****"
-gnome-terminal
-open5gs-amfd
+sudo systemctl start open5gs-amfd
 
 echo "**** Starting sgwud... ****"
-gnome-terminal
-open5gs-sgwud
+sudo systemctl start open5gs-sgwud
 
 echo "**** Starting upfd... ****"
-gnome-terminal
-open5gs-upfd
+sudo systemctl start open5gs-upfd
 
 echo "**** Starting hssd... ****"
-gnome-terminal
-open5gs-hssd
+sudo systemctl start open5gs-hssd
 
 echo "**** Starting pcrfd... ****"
-gnome-terminal
-open5gs-pcrfd
+sudo systemctl start open5gs-pcrfd
 
 echo "**** Starting nrfd... ****"
-gnome-terminal
-open5gs-nrfd
+sudo systemctl start open5gs-nrfd
 
 echo "**** Starting ausfd... ****"
-gnome-terminal
-open5gs-ausfd
+sudo systemctl start open5gs-ausfd
 
 echo "**** Starting udmd... ****"
-gnome-terminal
-open5gs-udmd
+sudo systemctl start open5gs-udmd
 
 echo "**** Starting pcfd... ****"
-gnome-terminal
-open5gs-pcfd
+sudo systemctl start open5gs-pcfd
 
 echo "**** Starting nssfd... ****"
-gnome-terminal
-open5gs-nssfd
+sudo systemctl start open5gs-nssfd
 
 echo "**** Starting bsfd... ****"
-gnome-terminal
-open5gs-bsfd
+sudo systemctl start open5gs-bsfd
 
 echo "**** Starting udrd... ****"
-gnome-terminal
-open5gs-udrd
+sudo systemctl start open5gs-udrd
 
 echo "**** Starting webui... ****"
-gnome-terminal
-open5gs-webui
+sudo systemctl start open5gs-webui
 
 echo
 echo
-echo "Done.."
+echo "Done"
+echo "Everything looks good to go!"
