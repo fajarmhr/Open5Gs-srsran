9acb960294bff956341c0b8e53b5e7c938f53a90
masddika <mf.mahardika@gmail.com>
2022-10-17 15:04:06 +0700

    edit zmq

--- ZMQ_based/intrahandov/enb.sh
+++ ZMQ_based/intrahandov/enb.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-LOG_ARGS="--log.all_level=debug"
-
-ENB_LOC=`locate /src/srsenb | grep /srsenb/src/srsenb`
-CONF_LOC=`locate enb.conf | grep intrahandov`
 RR_LOC=`locate rr.conf | grep intrahandov`
-RB_LOC=`locate rb.conf | grep intrahandov`
-SIB_LOC=`locate sib.conf | grep intrahandov`
 
-OTHER_ARGS="--enb_files.rr_config="${RR_LOC}"
---enb_files.rb_config="${RB_LOC}"
---enb_files.sib_config="${SIB_LOC}""
+OTHER_ARGS="--enb_files.rr_config="${RR_LOC}""
 
-sudo ${ENB_LOC} ${CONF_LOC} ${OTHER_ARGS} ${LOG_ARGS} $@
+sudo srsenb ./enb.conf ${OTHER_ARGS} $@
